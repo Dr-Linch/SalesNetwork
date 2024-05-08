733dfa4aa069f318badf44013c58ea62cadda018
Linch <dr.linch19@gmail.com>
2024-05-08 12:12:16 +0300

    Create Provider model and Product model

--- sales_network/models.py
+++ sales_network/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from users.models import NULLABLE
+from datetime import datetime, timedelta
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='')
+    model = models.CharField(max_length=100, verbose_name='')
+    launch_date = models.DateTimeField(default=datetime.now()+timedelta(weeks=2), verbose_name='')
+
+
+class Provider(models.Model):
+
+    LEVELS = (
+        ('FAC', 'factory'),
+        ('STO', 'store'),
+        ('IND', 'individual entepreneur')
+    )
+
+    level = models.CharField(max_length=3, choices=LEVELS)
+    name = models.CharField(max_length=100, verbose_name='Provide Name')
+    email = models.EmailField(verbose_name='Provide Email')
+    country = models.CharField(max_length=100, verbose_name='Provider Country')
+    citi = models.CharField(max_length=100, verbose_name='Provider Citi')
+    street = models.CharField(max_length=100, verbose_name='Provider Street')
+    house_number = models.IntegerField(verbose_name='Provider House Number')
+    products = models.ManyToManyField(Product)
+    self_provider = models.ForeignKey('self', on_delete=models.SET_NULL, **NULLABLE)
+    provider_debt = models.BigIntegerField(verbose_name='', **NULLABLE)
+    created_at = models.DateTimeField(default=datetime.now(), verbose_name='Creation time')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Provider'
+        verbose_name_plural = 'Providers'
